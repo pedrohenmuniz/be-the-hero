b108b6b7c7736f841bacb22f88d379d7926180bd
Pedro Muniz <pehensantos@gmail.com>
2020-03-28 15:20:07 -0300

    Mobile da aplicacao

---
+++ mobile/App.js
@@ -0,0 +1,27 @@
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+
+import React from 'react';
+
+import Routes from './src/routes';
+
+
+export default function App() {
+  return (
+    // <Text>Hello</Text>
+    <Routes/>
+  );
+}
+
+
+
+
+
+
+
+/**
+ * View utilizada pra blocos (como divs)
+ * Text para qualquer tipo de texto
+ * 
+ * 
+ */
